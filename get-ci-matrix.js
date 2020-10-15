ca846778be0f6539295b6270f8e620d2ce68b67f
Dico200 <dico.karssiens@gmail.com>
2020-10-15 20:10:39 +0200

    Try another thing (21)

--- get-ci-matrix.js
+++ get-ci-matrix.js
@@ -1,6 +1,6 @@
-// see https://github.com/actions/github-script/blob/main/action.yml
+// see https://github.com/actions/github-script
 // for info on arguments.
 
 module.exports = async function ({ github, context, io, core }) {
-    return { value: ["a", "b"] };
+    return { value: ["a", "b/c"] };
 };
