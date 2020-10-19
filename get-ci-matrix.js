8161a6f9b37e8a4d7a9ee1b799165223fad5ccff
Dico200 <dico.karssiens@gmail.com>
2020-10-19 09:58:55 +0200

    Try another thing (38)

--- get-ci-matrix.js
+++ get-ci-matrix.js
@@ -5,8 +5,5 @@ module.exports = async function ({github, context, io, core}) {
     // console.log("::set-output name=enabled::true");
     // console.log(`::set-output name=matrix::${JSON.stringify({value: ["a", "b/c"]})}`)
 
-    return {
-        enabled: true,
-        matrix: {value: ["a", "b/c"]}
-    };
+    return {value: ["a", "b/c"]};
 };
