1605fbb418f42bfdbade1203d117c07f082c2a2b
Dico200 <dico.karssiens@gmail.com>
2020-10-20 11:17:00 +0200

    Try another thing (43)

--- get-ci-matrix.js
+++ get-ci-matrix.js
@@ -2,6 +2,9 @@
 // for info on arguments.
 
 module.exports = async function ({github, context, io, core}) {
+    const { event, event_name, event_path } = context;
+    console.dir({ event, event_name, event_path }, { depth: 5 });
+
     // console.log("::set-output name=enabled::true");
     // console.log(`::set-output name=matrix::${JSON.stringify({value: ["a", "b/c"]})}`)
 
